365e67470d9a64a4f7e9bd3bb4e11cca7ffe8d1d
John Doe <johndoe@email.com>
2024-09-16 17:56:22 +0300

    new method was added

--- lab3.java
+++ lab3.java
@@ -1,7 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class lab3{
+public class lab3 {
+    public static List<String> getStudents() {
+        List<String> students = new ArrayList<>();
+        students.add("Mohammed");
+        students.add("Omar");
+        students.add("Ali");
+        students.add("Hassan");
+        return students;
+    }
     public static void main(String[] args) {
-        ArrayList<String> studentNames=new ArrayList<>();
+        List<String> students = getStudents();
+        
+        System.out.println("List of Students:");
+        for (String student : students) {
+            System.out.println(student);
+        }
     }
 }
