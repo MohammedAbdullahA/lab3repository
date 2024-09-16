52c81ec832222bee3256caf1e9bba7d10dcf083b
John Doe <johndoe@email.com>
2024-09-16 18:04:12 +0300

    new changes made and method added

--- lab3.java
+++ lab3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class lab3 {
@@ -17,5 +18,20 @@ public class lab3 {
         for (String student : students) {
             System.out.println(student);
         }
+        int groupSize = 2; // Change this to set the size of each group
+        List<List<String>> groups = createRandomGroups(students, groupSize);
+        
+        System.out.println("Random Groups of Students:");
+        for (List<String> group : groups) {
+            System.out.println(group);
+        }
+    }
+    public static List<List<String>> createRandomGroups(List<String> students, int groupSize) {
+        Collections.shuffle(students);
+        List<List<String>> groups = new ArrayList<>();
+        for (int i = 0; i < students.size(); i += groupSize) {
+            groups.add(new ArrayList<>(students.subList(i, Math.min(i + groupSize, students.size()))));
+        }
+        return groups;
     }
 }
